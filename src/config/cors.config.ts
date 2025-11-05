466b225530157ddfc2dd38cf5a3044ea53bf2134
Max <paskom323323@gmail.com>
2025-11-05 22:25:47 +0300

    chore(config): add CORS configuration

---
+++ src/config/cors.config.ts
@@ -0,0 +1,9 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
+
+export const getCorsConfig = (configService: ConfigService): CorsOptions => {
+  return {
+    origin: configService.getOrThrow<string>('HTTP_CORS'),
+    credentials: true,
+  };
+};
