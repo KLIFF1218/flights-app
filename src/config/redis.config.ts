5b6c294930f8927e8a5f59352a9b6e3f6432ad1e
Max <paskom323323@gmail.com>
2025-11-06 21:26:29 +0300

    chore(config): add redis configuration

---
+++ src/config/redis.config.ts
@@ -0,0 +1,12 @@
+import { ConfigService } from '@nestjs/config';
+import { RedisOptions } from 'ioredis';
+
+const redisConfig = (configService: ConfigService): RedisOptions => {
+  return {
+    host: configService.getOrThrow<string>('REDIS_HOST'),
+    port: configService.getOrThrow<number>('REDIS_PORT'),
+    password: configService.getOrThrow<string>('REDIS_PASSWORD'),
+  };
+};
+
+export { redisConfig };
