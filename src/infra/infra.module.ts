0c547acaeec2366907104e56a21e58de098be70c
Max <paskom323323@gmail.com>
2025-11-06 21:30:16 +0300

    feat(infra): add InfraModule for connect PrismaModule

---
+++ src/infra/infra.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [PrismaService],
+})
+export class InfraModule {}
