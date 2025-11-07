4d3dc6dfdb111017820a8da5b25d5b41b69752df
Max <paskom323323@gmail.com>
2025-11-07 17:47:05 +0300

    feat(module): organize module imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { FlightsModule } from './modules/flights/flights.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { PaymentsModule } from './modules/payment/payment.module';
+import { AirportsModule } from './modules/airports/airports.module';
+import { InfraModule } from './infra/infra.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
+    FlightsModule,
+    BookingsModule,
+    PaymentsModule,
+    AirportsModule,
+    InfraModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
