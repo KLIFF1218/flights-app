ae98de1ecf866cdab717e5af6415f9952119ec70
Max <paskom323323@gmail.com>
2025-11-09 21:00:49 +0300

    feat(bullmq): add BullMQ configuration

---
+++ src/config/bullmq.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { QueueOptions } from 'bullmq';
+import { redisConfig } from './redis.config';
+
+export const getBullmqConfig = (configService: ConfigService): QueueOptions => {
+  return {
+    connection: {
+      maxRetriesPerRequest: 5,
+      retryStrategy: (times) => Math.min(times * 50, 2000),
+      ...redisConfig(configService),
+    },
+    prefix: configService.getOrThrow<string>('QUEUE_PREFIX'),
+  };
+};
