f3c6db63b4973e11ff1bcf74954b6ddb9e64763e
Max <paskom323323@gmail.com>
2025-11-18 21:17:05 +0300

    docs(flighs): add Swagger documentation for FlightsController

--- src/modules/flights/flights.controller.ts
+++ src/modules/flights/flights.controller.ts
@@ -14,12 +14,25 @@ import { Protected } from 'src/common/decorators/protected.decorator';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { RolesGuard } from 'src/common/guards';
 import { Flight } from '@prisma/client';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('flights')
 export class FlightsController {
   constructor(private readonly flightService: FlightsService) {}
 
   @Post('search')
+  @ApiOperation({
+    summary: 'Поиск доступных рейсов',
+    description:
+      'Позволяет искать рейсы по направлению, дате, количеству пассажиров и классу обслуживания.',
+  })
+  @ApiBody({ type: SearchFlightsDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Успешный поиск. Возвращает список найденных рейсов.',
+    // type: [],
+  })
+  @ApiResponse({ status: 404, description: 'Рейсы не найдены' })
   async search(@Body() data: SearchFlightsDto): Promise<Flight[]> {
     return await this.flightService.search(data);
   }
