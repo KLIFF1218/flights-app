d069db3a28d75e1768a48a15afb4d825013fa71c
Max <paskom323323@gmail.com>
2025-11-20 19:37:07 +0300

    feat(app): integrate Pino logger, Redis module, Mail module and global rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,20 @@ import { PaymentsModule } from './modules/payment/payment.module';
 import { AirportsModule } from './modules/airports/airports.module';
 import { InfraModule } from './infra/infra.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MailModule } from './libs/mail/mail.module';
+import { PinoLoggerModule } from './common/logger/pino-logger.module';
+import { RedisModule } from './redis/redis.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RateLimiterService } from './libs/rate-limiter/rate-limiter-redis.service';
+import { RateLimitGuard } from './common/guards/rate-limit.guard';
 
 @Module({
   imports: [
+    PinoLoggerModule.forRoot({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      prettyPrint: process.env.NODE_ENV !== 'production',
+      name: 'MaxAirline',
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -20,6 +31,15 @@ import { AuthModule } from './modules/auth/auth.module';
     AirportsModule,
     InfraModule,
     AuthModule,
+    MailModule,
+    RedisModule,
+  ],
+  providers: [
+    RateLimiterService,
+    {
+      provide: APP_GUARD,
+      useClass: RateLimitGuard,
+    },
   ],
 })
 export class AppModule {}
