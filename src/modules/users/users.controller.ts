2ac8920b89d1edcdfdea93634d6db2e8dcaa4e3a
Max <paskom323323@gmail.com>
2025-11-20 19:38:47 +0300

    feat(users): add Swagger documentation and DTOs to UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,28 +1,51 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('')
+  @Get()
+  @ApiOperation({ summary: 'Получить список всех пользователей' })
+  @ApiResponse({ status: 200, type: [UserResponseDto] })
   async getAll() {
-    return this.usersService.getAll();
+    const users = await this.usersService.getAll();
+    return { data: users };
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить пользователя по ID' })
+  @ApiParam({ name: 'id', description: 'ID пользователя' })
+  @ApiResponse({ status: 200, type: UserResponseDto })
+  @ApiResponse({ status: 404, description: 'Пользователь не найден' })
   async getById(@Param('id') id: string) {
     return this.usersService.getById(id);
   }
 
+  // поменять чтобы пароль не возращало даже хэш!!!
+
   @Post()
-  async create(
-    @Body() data: { email: string; password: string; fullName: string },
-  ) {
+  @ApiOperation({ summary: 'Создать нового пользователя' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, type: UserResponseDto })
+  async create(@Body() data: CreateUserDto) {
     return this.usersService.create(data);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Удалить пользователя' })
+  @ApiParam({ name: 'id', description: 'ID пользователя' })
+  @ApiResponse({ status: 200, description: 'Пользователь удалён' })
+  @ApiResponse({ status: 404, description: 'Пользователь не найден' })
   async remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
