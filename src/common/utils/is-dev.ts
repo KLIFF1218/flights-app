a5cfcde41c2c563f2880be81a08929ca7f8fbb66
Max <paskom323323@gmail.com>
2025-11-25 22:08:41 +0300

    refactor(env): remove dotenv import and usage

--- src/common/utils/is-dev.ts
+++ src/common/utils/is-dev.ts
@@ -1,7 +1,4 @@
 import { ConfigService } from '@nestjs/config';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const isDev = (configService: ConfigService) => {
   return configService.getOrThrow<string>('NODE_ENV') === 'development';
