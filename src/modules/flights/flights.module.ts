b1b27df3daed9c0b60ec0ac46ebfaa78026478f7
Max <paskom323323@gmail.com>
2025-12-16 20:45:30 +0300

    feat(flights): add RedisModule to FlightsModule imports

--- src/modules/flights/flights.module.ts
+++ src/modules/flights/flights.module.ts
@@ -3,8 +3,10 @@ import { FlightsService } from './flights.service';
 import { FlightsController } from './flights.controller';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
 import { FlightsCacheService } from './flights-cache.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
+  imports: [RedisModule],
   controllers: [FlightsController],
   providers: [FlightsService, PrismaService, FlightsCacheService],
 })
