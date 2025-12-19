7a1cda16339e54f203a6a82a9cd98cf44d3ddfc9
Max <paskom323323@gmail.com>
2025-12-19 21:10:55 +0300

    feat(flights): add Amadeus service integration

--- src/modules/flights/flights.module.ts
+++ src/modules/flights/flights.module.ts
@@ -4,10 +4,16 @@ import { FlightsController } from './flights.controller';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
 import { FlightsCacheService } from './flights-cache.service';
 import { RedisModule } from 'src/redis/redis.module';
+import { AmadeusService } from './amadeus.service';
 
 @Module({
   imports: [RedisModule],
   controllers: [FlightsController],
-  providers: [FlightsService, PrismaService, FlightsCacheService],
+  providers: [
+    FlightsService,
+    PrismaService,
+    FlightsCacheService,
+    AmadeusService,
+  ],
 })
 export class FlightsModule {}
