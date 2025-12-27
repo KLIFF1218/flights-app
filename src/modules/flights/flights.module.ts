70e224288f95afac345310b2bc24a44df8b05af2
Max <paskom323323@gmail.com>
2025-12-27 20:31:59 +0300

    refactor(flights): update module with pricing and renamed cache service

--- src/modules/flights/flights.module.ts
+++ src/modules/flights/flights.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { FlightsService } from './flights.service';
 import { FlightsController } from './flights.controller';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
-import { FlightsCacheService } from './flights-cache.service';
 import { RedisModule } from 'src/redis/redis.module';
 import { AmadeusService } from './amadeus.service';
+import { FlightsPricingService } from './flight-pricing.service';
+import { FlightPricingController } from './flight-pricing.controller';
+import { FlightsSearchStore } from './flights-cache.service';
 
 @Module({
   imports: [RedisModule],
-  controllers: [FlightsController],
+  controllers: [FlightsController, FlightPricingController],
   providers: [
     FlightsService,
     PrismaService,
-    FlightsCacheService,
     AmadeusService,
+    FlightsPricingService,
+    FlightsSearchStore,
   ],
 })
 export class FlightsModule {}
