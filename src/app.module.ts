1587d86912e777b25b852ebce33ef3837f5d13e1
Max <paskom323323@gmail.com>
2025-12-30 23:11:44 +0300

    feat(app): add seat map module and switch to async pino logger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { FlightsModule } from './modules/flights/flights.module';
 import { BookingsModule } from './modules/bookings/bookings.module';
@@ -8,21 +8,44 @@ import { AirportsModule } from './modules/airports/airports.module';
 import { InfraModule } from './infra/infra.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MailModule } from './libs/mail/mail.module';
-import { PinoLoggerModule } from './common/logger/pino-logger.module';
 import { RedisModule } from './redis/redis.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RateLimiterService } from './libs/rate-limiter/rate-limiter-redis.service';
 import { RateLimitGuard } from './common/guards/rate-limit.guard';
+import { SeatMapModule } from './modules/seatmaps/seatmap.module';
+import { LoggerModule } from 'nestjs-pino';
+import { isDev } from './common/utils';
 
 @Module({
   imports: [
-    PinoLoggerModule.forRoot({
-      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-      prettyPrint: process.env.NODE_ENV !== 'production',
-      name: 'MaxAirline',
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const isDevelop = isDev(config);
+
+        return {
+          pinoHttp: {
+            level: config.get('LOG_LEVEL', 'info'),
+
+            transport: isDevelop
+              ? {
+                  target: 'pino-pretty',
+                  options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: 'pid,hostname',
+                  },
+                }
+              : undefined,
+
+            genReqId: (req) =>
+              req.headers['x-request-id'] ?? crypto.randomUUID(),
+          },
+        };
+      },
     }),
     UsersModule,
     FlightsModule,
@@ -33,6 +56,7 @@ import { RateLimitGuard } from './common/guards/rate-limit.guard';
     AuthModule,
     MailModule,
     RedisModule,
+    SeatMapModule,
   ],
   providers: [
     RateLimiterService,
