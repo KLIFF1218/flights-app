5c9911886293893d5ac74ac842251d0f3569bb2d
Max <paskom323323@gmail.com>
2026-01-04 21:52:21 +0300

    feat(config): export additional configuration modules for improved setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,7 @@
 export * from './redis.config';
 export * from './jwt.config';
+export * from './bullmq.config';
+export * from './cors.config';
+export * from './mailer.config';
+export * from './swagger.config';
+export * from './yookassa.config';
