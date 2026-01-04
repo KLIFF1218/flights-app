23c2e0d88d66c6301554f11178be38623a660013
Max <paskom323323@gmail.com>
2026-01-04 21:52:43 +0300

    feat(infra): update InfraModule imports to include additional modules

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './db/prisma/prisma.module';
+import { HttpModule } from './http/http.module';
+import { RateLimiterModule } from './rate-limiter/rate-limiter.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [PrismaService],
+  imports: [PrismaModule, HttpModule, RateLimiterModule, MailModule],
 })
 export class InfraModule {}
