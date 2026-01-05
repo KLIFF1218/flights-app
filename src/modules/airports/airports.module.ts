248ddb53c11bb797dc7fd4c7fc0180401f091ef1
Max <paskom323323@gmail.com>
2026-01-05 23:06:26 +0300

    feat(airports): add imports for FlightsModule and InfraModule in AirportsModule

--- src/modules/airports/airports.module.ts
+++ src/modules/airports/airports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AirportsService } from './airports.service';
 import { AirportsController } from './airports.controller';
+import { FlightsModule } from '../flights/flights.module';
+import { InfraModule } from 'src/infra/infra.module';
 
 @Module({
+  imports: [FlightsModule, InfraModule],
   controllers: [AirportsController],
   providers: [AirportsService],
 })
