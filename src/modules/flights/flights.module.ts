15afda8f0d9b6bea070c4404ad23f53b86764db9
Max <paskom323323@gmail.com>
2026-01-13 21:57:57 +0300

    refactor(flights.module): correct PrismaService import path and add missing services for improved functionality

--- src/modules/flights/flights.module.ts
+++ src/modules/flights/flights.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FlightsService } from './flights.service';
 import { FlightsController } from './flights.controller';
-import { PrismaService } from 'src/infra/prisma/prisma.service';
+import { PrismaService } from 'src/infra/db/prisma/prisma.service';
 import { RedisModule } from 'src/redis/redis.module';
 import { AmadeusService } from './amadeus.service';
 import { FlightsPricingService } from './flight-pricing.service';
 import { FlightPricingController } from './flight-pricing.controller';
 import { FlightsSearchStore } from './flights-cache.service';
+import { AmadeusAuthService } from '../../infra/http/amadeus/amadeus-auth.service';
+import { AmadeusHttpClient } from 'src/infra/http/amadeus/amadeus-http-client.service';
 
 @Module({
   imports: [RedisModule],
@@ -17,6 +19,10 @@ import { FlightsSearchStore } from './flights-cache.service';
     AmadeusService,
     FlightsPricingService,
     FlightsSearchStore,
+    AmadeusAuthService,
+    AmadeusHttpClient,
+    FlightsSearchStore,
   ],
+  exports: [AmadeusService, AmadeusHttpClient, FlightsSearchStore],
 })
 export class FlightsModule {}
