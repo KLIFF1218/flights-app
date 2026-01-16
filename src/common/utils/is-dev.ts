0368c8595f78e95198ccb6c2259549b9bffe68e4
Max <paskom323323@gmail.com>
2026-01-16 21:22:51 +0300

    fix(is-dev): change import to use type for ConfigService

--- src/common/utils/is-dev.ts
+++ src/common/utils/is-dev.ts
@@ -1,4 +1,4 @@
-import { ConfigService } from '@nestjs/config';
+import type { ConfigService } from '@nestjs/config';
 
 export const isDev = (configService: ConfigService) => {
   return configService.getOrThrow<string>('NODE_ENV') === 'development';
