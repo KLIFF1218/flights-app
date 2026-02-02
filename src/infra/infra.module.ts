b082d40ecc639c34e2828ba8d0e5489dcfaa091f
Max <paskom323323@gmail.com>
2026-02-02 19:53:23 +0300

    feat: add PdfModule and S3Module to InfraModule imports

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -3,8 +3,10 @@ import { PrismaModule } from './db/prisma/prisma.module';
 import { HttpModule } from './http/http.module';
 import { RateLimiterModule } from './rate-limiter/rate-limiter.module';
 import { MailModule } from './mail/mail.module';
+import { PdfModule } from './pdf/pdf.module';
+import { S3Module } from './storage/s3.module';
 
 @Module({
-  imports: [PrismaModule, HttpModule, RateLimiterModule, MailModule],
+  imports: [PrismaModule, HttpModule, RateLimiterModule, MailModule, PdfModule, S3Module],
 })
 export class InfraModule {}
